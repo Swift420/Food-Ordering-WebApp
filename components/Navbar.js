379e54fdbc9e9adb02aba367ccbb0ed4658001d3
Swift420 <61661828+Swift420@users.noreply.github.com>
2022-01-07 16:18:31 +0200

    1st commit

---
+++ components/Navbar.js
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import React from 'react'
+import styles from "../styles/Navbar.module.css"
+function Navbar() {
+    return (
+        <div className={styles.container}>
+           <div className={styles.item}>
+           <div className={styles.callButton}>
+               <Image src="/img/telephone.png" width="32" height="32"/>
+           </div>
+           <div className={styles.texts}>
+           <div className={styles.text}>ORDER NOW!</div>
+           <div className={styles.text}>085 345 765</div>
+           </div>
+
+           </div>
+           <div className={styles.item}>
+               <ul className={styles.list}>
+                   <li className={styles.listItem}>HomePage</li>
+                   <li className={styles.listItem}>Products</li>
+                   <li className={styles.listItem}>Menu</li>
+                   <Image src="/img/logo.png" width="160px" height="69px"/>
+                   <li className={styles.listItem}>Events</li>
+                   <li className={styles.listItem}>Blog</li>
+                   <li className={styles.listItem}>Contact</li>
+                 
+               </ul>
+           </div>
+           <div className={styles.item}>
+               <div className={styles.cart}>
+               <Image src="/img/cart.png" width="30px" height="30px"/>
+               <div className={styles.counter}>2</div>
+               </div>
+           </div>
+
+        </div>
+    )
+}
+
+export default Navbar
