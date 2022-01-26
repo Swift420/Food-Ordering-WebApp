67044162bec2b291bbc57dd25e48ef033532deb9
Swift420 <61661828+Swift420@users.noreply.github.com>
2022-01-26 22:35:03 +0200

    Add product p1

---
+++ components/Add.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Add() {
+  return <div>
+      hello
+  </div>;
+}
+
+export default Add;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import axios from 'axios'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Add from '../components/Add'
+import AddButton from '../components/AddButton'
 import Featured from '../components/Featured'
 import PizzaCard from '../components/PizzaCard'
 import PizzaList from '../components/PizzaList'
 import styles from '../styles/Home.module.css'
 
-export default function Home({ pizzaList} ) {
+export default function Home({ pizzaList, admin} ) {
 
-
+  const [close, setClose] = useState(true)
   
   return (
     <div className={styles.container}>
@@ -20,18 +22,27 @@ export default function Home({ pizzaList} ) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose}/>}
       <PizzaList pizzaList={pizzaList}/>
+      {!close && <Add setClose ={setClose} /> }
       
     </div>
   )
 }
 
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if(myCookie.token === process.env.TOKEN){
+    admin = true;
+  }
   const res = await axios.get("http://localhost:3000/api/products");
   return {
     props: {
-      pizzaList: res.data
+      pizzaList: res.data,
+      admin
     }
   }
 }
