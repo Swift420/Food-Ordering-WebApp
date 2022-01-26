016053a8eb5b3c459d56de1418f8bff25551ae84
Swift420 <61661828+Swift420@users.noreply.github.com>
2022-01-26 18:08:47 +0200

    API integration

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -1,30 +1,23 @@
-import React from 'react'
-import styles from "../styles/PizzaList.module.css"
-import PizzaCard from './PizzaCard'
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
 
-function PizzaList() {
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
-            <p className={styles.desc}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
 
-         <div className={styles.wrapper}>
-             <PizzaCard />
-             <PizzaCard />
-             <PizzaCard />
-             <PizzaCard />
-             <PizzaCard />
-             <PizzaCard />
-             <PizzaCard />
+const PizzaList = ({ pizzaList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
+        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit.
+      </p>
+      <div className={styles.wrapper}>
+        {pizzaList.map((pizza) => (
+          <PizzaCard key={pizza._id} pizza={pizza} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-         </div>
-
-        </div>
-    )
-}
-
-export default PizzaList
+export default PizzaList;
